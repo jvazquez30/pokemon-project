236474d2696ef1364158c7914af769e6920d54d5
jvazquez30 <josevazquez881@gmail.com>
2025-06-06 23:56:52 -0500

    add params

--- src/app/generation/[name]/page.tsx
+++ src/app/generation/[name]/page.tsx
@@ -4,6 +4,9 @@ import { useParams } from "next/navigation"
 // this will show the generation information
 
 export default function GenerationPage() {
+    const params = useParams()
+    
+
     <div>
         Adding Generation info:
             Cities:
