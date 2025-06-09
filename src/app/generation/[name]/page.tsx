796cb0c193b62961e57682d4b003519285405879
jvazquez30 <josevazquez881@gmail.com>
2025-06-08 23:57:28 -0500

    state

--- src/app/generation/[name]/page.tsx
+++ src/app/generation/[name]/page.tsx
@@ -1,15 +1,18 @@
 import { useParams } from "next/navigation"
+import { useState } from "react"
 
 
 // this will show the generation information
 
 export default function GenerationPage() {
     const params = useParams()
-    
-
+    const [info, setinfo] = useState()
+return (
     <div>
         Adding Generation info:
             Cities:
             Country:
     </div>
+
+)
 }
