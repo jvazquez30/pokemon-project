8a367af7af15fff578e0757ab5829ce0081a5367
jvazquez30 <josevazquez881@gmail.com>
2025-11-11 21:22:09 -0800

    removed TypeInfo from types page

--- src/app/types/page.tsx
+++ src/app/types/page.tsx
@@ -1,6 +1,5 @@
 'use client'
 
-import { TypeInfo } from "../../../services/information";
 import TypeList from "../components/TypeList";
 import Link from "next/link";
 import Image from "next/image";
