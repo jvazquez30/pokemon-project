e2c31e410f0d6ce437351791a1a065d7d9f3d9e1
jvazquez30 <josevazquez881@gmail.com>
2025-12-19 23:36:00 -0800

    added move list limit 30

---
+++ src/app/moves/movesInfo.ts
@@ -0,0 +1,32 @@
+const POKEMON_API_URL = 'https://pokeapi.co/api/v2';
+
+
+export interface MoveInformation {
+  name: string
+}
+
+export interface Moves {
+  name: string
+  url: string
+}
+
+export interface MoveList {
+  count: number;
+  next: string;
+  previous: string | null;
+  results: Moves[];
+}
+
+
+
+
+export async function getAllMoves(limit: number = 30, offset: number = 200): Promise<MoveList>  {
+  const response = await fetch(`${POKEMON_API_URL}/move?offset=${offset}&limit=${limit}`) 
+
+    if (!response.ok) {
+      throw new Error("Failed to retrieve moves")
+    }
+
+    return response.json()
+} 
+
--- src/app/moves/page.tsx
+++ src/app/moves/page.tsx
@@ -1,22 +1,48 @@
-import React from "react"; 
+"use client"
+
+import React from "react";
 import Link from "next/link";
 import Image from "next/image";
+import { MoveList, getAllMoves } from "./movesInfo";
+import { useState, useEffect } from "react";
 
 
 
 export default function MovesPage() {
+  const [moves, setMoves] = useState<MoveList | null>(null);
 
- return (
+  useEffect(() => {
+    async function fetchMoves() {
+      try {
+        const data = await getAllMoves();
+        setMoves(data)
+      } catch (error) {
+        console.error("Failed to get moves data")
+      }
+    }
+    fetchMoves()
+  }, [])
 
-  <div>
-     <header className="flex justify-center items-center w-full bg-red-600">
-                <Image src="/pokeTrainer.png" alt="Pokemon" width={300} height={300} />
-                <Link href="/">Home</Link>
-            </header>
+  return (
 
+    <div>
+      <header className="flex justify-center items-center w-full bg-red-600">
+        <Image src="/pokeTrainer.png" alt="Pokemon" width={300} height={300} />
+        <Link href="/">Home</Link>
+      </header>
 
-    Moves will display here
-  </div>
+      <div>
+        <h1>
+        {moves ? (
+          moves.results.map((move) => (
+            <p key={move.name}>{move.name}</p>
+          ))
+        ) : (
+          <p>Loading Moves...</p>
+        )}
+        </h1>
+      </div>
+    </div>
 
- )
+  )
 }
