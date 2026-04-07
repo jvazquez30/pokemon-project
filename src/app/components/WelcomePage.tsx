f9d31af4694a28db13428947c9d5d8b30963cb38
jvazquez30 <josevazquez881@gmail.com>
2026-04-07 14:34:52 -0700

    changed sm view into col

--- src/app/components/WelcomePage.tsx
+++ src/app/components/WelcomePage.tsx
@@ -8,14 +8,14 @@ export default function WelcomePage() {
                 <h1 className="font-bold text-4xl ">Welcome to the PokeTrainer</h1>
                 <p>This is a beginner friendly site to help you learn about Pokemon</p>
             </div>
-            <div className="flex flex-col items-center justify-center ">
-                <div className="flex gap-2">
-                    <Link href="/search" className="bg-blue-500 text-white p-2 rounded-md">Search For Pokemon</Link>
-                    <Link href="/pokedex" className="bg-red-500 text-white p-2 rounded-md">Pokedex</Link>
-                    <Link href="/generation" className="bg-blue-500 text-white p-2 rounded-md">Generation</Link>
-                    <Link href="/types" className="bg-blue-500 text-white p-2 rounded-md">Types</Link>
-                    <Link href='/moves' className="bg-blue-500 text-white p-2 rounded-md">Moves</Link>
-                    <Link href='/items' className="bg-blue-500 text-white p-2 rounded-md">Items</Link>
+            <div className="flex items-center justify-center ">
+                <div className="gap-2 grid grid-cols-1">
+                    <Link href="/search" className="bg-blue-500 text-white p-2 rounded-md text-center">Search For Pokemon</Link>
+                    <Link href="/pokedex" className="bg-red-500 text-white p-2 rounded-md text-center">Pokedex</Link>
+                    <Link href="/generation" className="bg-blue-500 text-white p-2 rounded-md text-center">Generation</Link>
+                    <Link href="/types" className="bg-blue-500 text-white p-2 rounded-md text-center">Types</Link>
+                    <Link href='/moves' className="bg-blue-500 text-white p-2 rounded-md text-center">Moves</Link>
+                    <Link href='/items' className="bg-blue-500 text-white p-2 rounded-md text-center">Items</Link>
                 </div>
             </div>
         </div>
